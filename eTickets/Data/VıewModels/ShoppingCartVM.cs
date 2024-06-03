501807cca282a4f55d6eee79059783f1720b4e32
Mahmud Doğan <mahmud.dogan164@gmail.com>
2024-06-03 22:14:37 +0300

    Added Orders & cart

---
+++ eTickets/Data/VıewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using eTickets.Data.Cart;
+
+namespace eTickets.Data.VıewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,7 +1,9 @@
 using eTickets.Data;
+using eTickets.Data.Cart;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -34,7 +36,13 @@ namespace eTickets
             services.AddScoped<IActorServices, ActorServices>();
             services.AddScoped<IProducersServices, ProducersServices>();
             services.AddScoped<ICinemasService, CinemasService>();
-            services.AddScoped<IMoviesService, MoviesService>();    
+            services.AddScoped<IMoviesService, MoviesService>();
+            services.AddScoped<IOrdersService, OrdersService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+
+            services.AddSession();
 
             services.AddControllersWithViews();
         }
@@ -56,6 +64,7 @@ namespace eTickets
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseSession();
 
             app.UseAuthorization();
 
